5a93c3183355bfb2d97c1cfb2b1cefabc5febcb0
Stephen <pena731.junk@gmail.com>
2023-01-15 18:37:54 -0500

    project control flow

--- index.js
+++ index.js
@@ -1,11 +1,50 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(num){
   // Write your code here!
+  
+  if(num <= 400){
+    return("This one is on me!");
+    } else if (num > 400 && num < 2000){
+      return("That will be twenty bucks.");
+    } else if (num > 2000 && num < 2500){
+      return("I will gladly take your thirty bucks.");
+    } else if (num > 2500){
+      return("No can do.");
+    }
+    
 }
+scuberGreetingForFeet(199);
+scuberGreetingForFeet(1500);
+scuberGreetingForFeet(2001);
+scuberGreetingForFeet(2501);
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+/*else if(distance === 400 || < 2000 ) */
 
-function switchOnCharmFromTip(){
+function ternaryCheckCity(city){
   // Write your code here!
+  const message = city === 'NYC' ? "Ok, sounds good." : "No go.";
+  return (message);
+
 }
+ternaryCheckCity('NYC')
+ternaryCheckCity('Pittsburgh')
+
+
+function switchOnCharmFromTip(tip){
+  // Write your code here!
+  let response;
+switch (tip){
+  case 'generous':
+        response = "Thank you so much.";
+        break;
+  case 'not as generous':
+        response = "Thank you.";
+        break;
+  case 'thanks for everything':
+        response = "Bye.";
+        break;
+}
+return (response);
+}
+switchOnCharmFromTip('generous');
+switchOnCharmFromTip('not as generous');
+switchOnCharmFromTip('thanks for everything');
